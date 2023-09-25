0bcd8059a9f33c702aec603b767c5d78a6f30e50
N S Meenakshi <130451378+MeenakshiNS@users.noreply.github.com>
2023-09-25 11:26:53 +0530

    Update Message.jsx

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,18 +1,4 @@
-// import React from 'react'
-// import {Alert} from 'react-bootstrap';
 
-// const Message = ({variant,children}) => {
- 
-//     <Alert variant={variant}>
-//         {children}
-//     </Alert>
-// };
-
-// Message.defaultProps={
-//     variant:'info',
-// };
-
-// export default Message
 
 
 import React from 'react';
