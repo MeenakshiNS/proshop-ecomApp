84894ff1454a45f3a76289cb66a95e9d91a17ec8
N S Meenakshi <130451378+MeenakshiNS@users.noreply.github.com>
2023-09-25 11:36:06 +0530

    Update cartUtils.js

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -3,13 +3,13 @@ export const addDecimals = (num) =>{
 }
 
 export const updateCart = (state) => {
-    //calculate items price
+   
     state.itemsPrice =addDecimals( state.cartItems.reduce((acc,item)=>acc+item.price *item.qty,0));
-    //calculate shipping price(if order is over $100 then free, else $10 shipping charge)
+    
     state.shippingPrice=addDecimals(state.itemsPrice >100 ?0:10);
-    //calculate tax price (15% tax)
+    
     state.taxPrice =addDecimals(Number((0.15* state.itemsPrice).toFixed(2)))
-    //calculate total price
+    
     state.totalPrice=(
         Number(state.itemsPrice)+
         Number(state.shippingPrice)+
